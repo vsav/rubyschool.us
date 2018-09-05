90a58be7111ddaec47ed7578502dfa4a83dea11f
Vadim <vsavdev@gmail.com>
2018-09-05 19:54:05 +0300

    File output

--- lesson20/barbershop.rb
+++ lesson20/barbershop.rb
@@ -4,3 +4,20 @@ get '/' do
   erb :index
 end
 
+post '/' do
+  @title = "Thank you!"
+  @name = params[:name]
+  @phone = params[:phone]
+  @date = params[:date]
+
+  @message = "#{@name}, we'll be waiting for you at #{@date}"
+
+  f = File.open('visits.txt', 'a')
+  f.write "Name: #{@name}, Phone: #{@phone}, Visit date: #{@date} \n"
+  f.close
+  
+  erb :welcome
+
+end
+
+
